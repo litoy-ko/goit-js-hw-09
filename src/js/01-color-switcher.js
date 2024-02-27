8cb8c525a779a5ce7aa9441fe2ceb76611b1044a
litoy-ko <litoys.ark2nd@gmail.com>
2024-02-27 23:46:46 +0800

    initial edit
    initial edit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,7 @@
+function getRandomHexColor() {
 
+}
+
+const body = document.querySelector("body");
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("data-stop");
---
+++ src/js/02-timer.js
@@ -0,0 +1,7 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+
+const refs = {
+    
+}
