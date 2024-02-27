359befa34a617f26ff8e1eb9de82eab66bd071e6
litoy-ko <144130729+litoy-ko@users.noreply.github.com>
2024-02-27 15:47:29 +0000

    Deploying to gh-pages from @ litoy-ko/goit-js-hw-09@8cb8c525a779a5ce7aa9441fe2ceb76611b1044a 🚀

---
+++ 01-color-switcher.74873b95.js
@@ -0,0 +1,2 @@
+document.querySelector("body"),document.querySelector("button[data-start]"),document.querySelector("data-stop");
+//# sourceMappingURL=01-color-switcher.74873b95.js.map
---
+++ 01-color-switcher.f359c0aa.js
@@ -0,0 +1,2 @@
+document.querySelector("body"),document.querySelector("button[data-start]"),document.querySelector("data-stop");
+//# sourceMappingURL=01-color-switcher.f359c0aa.js.map
