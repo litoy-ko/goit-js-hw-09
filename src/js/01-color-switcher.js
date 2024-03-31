a03ef5ea72218c78425c15d8efe986c522209462
litoy-ko <litoys.ark2nd@gmail.com>
2024-03-31 17:05:36 +0800

    task 1 commit
    task 1 commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,27 +1,23 @@
+console.log("ahsdjfa");
 function getRandomHexColor() {
-	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-	//#123456
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
-const body = document.querySelector("body");
 const startBtn = document.querySelector("button[data-start]");
 const stopBtn = document.querySelector("button[data-stop]");
-let colorChangeInterval = null;
 stopBtn.disabled = true;
+let timerID = null;
 
-function startColorChange() {
-	colorChangeInterval = setInterval(() => {
-		body.style.backgroundColor = getRandomHexColor();
-		startBtn.disabled = true;
+startBtn.addEventListener("click", () => {
+	timerID = setInterval(() => {
+		document.body.style.backgroundColor = getRandomHexColor();
 		stopBtn.disabled = false;
-	}, 1000);
-}
+		startBtn.disabled = true;
+	}, 1000)
+})
 
-function stopColorChange() {
-	clearInterval(colorChangeInterval);
+stopBtn.addEventListener("click", () => {
+	clearInterval(timerID);
 	startBtn.disabled = false;
-	stopBtn.diabled = true;
-}
-
-startBtn.addEventListener("click", startColorChange);
-stopBtn.addEventListener("click", stopColorChange);
+	stopBtn.disabled = true;
+})
